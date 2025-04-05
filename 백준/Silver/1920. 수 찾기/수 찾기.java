e705513b7a983782edd1ebc6f3003bf5b97bbe2a
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2025-04-05 22:40:13 +0900

    [Silver IV] Title: 수 찾기, Time: 776 ms, Memory: 48448 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,21 +5,40 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Integer[] listA = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
-        
-        // sort
-        Arrays.sort(listA);
-        
+        Integer[] array = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
         int M = Integer.parseInt(br.readLine());
-        Integer[] listB = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+        Integer[] query = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
         
-        for (int unit : listB) {
-            int findIndex = Arrays.binarySearch(listA, unit);
-            if (findIndex >= 0) {
-                System.out.println("1");
-            } else {
-                System.out.println("0");
-            }
+        // 정렬
+        Arrays.sort(array);
+        
+        StringBuilder response = new StringBuilder();
+        for (int q : query) {
+            int answer = binarySearch(N, array, q);
+            response.append(answer).append("\n");
         }
+        
+        System.out.print(response);
+       
+    }
+    private static int binarySearch(int N, Integer[] array, int k) {
+        int start = 0;
+        int end = N-1;
+        
+        while (start <= end) {
+            int mid = (start+end) / 2;
+            if (array[mid] == k) {
+                return 1;
+            }
+            
+            if (array[mid] < k) {
+                start = mid + 1;
+            } else {
+                end = mid -1;
+            }            
+        }
+        
+        return 0;
+        
     }
 }
