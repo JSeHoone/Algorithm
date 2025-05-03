2d95b0f56801788d5d01ca3fc4f4f5a3383352db
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2025-05-03 21:11:23 +0900

    [Gold IV] Title: N-Queen, Time: 5360 ms, Memory: 14556 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,13 +1,14 @@
 import java.io.*;
 
+
 public class Main {
     private static int N;
-    private static int[][] board;
+    private static int[] board;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        board = new int[N][N];
+        board = new int[N];
 
         System.out.println(dfs(0));
     }
@@ -18,43 +19,27 @@ public class Main {
         }
 
         int ans = 0;
-        for (int col =0; col < N; col++) {
-            if (isPossible(row, col)) {
-                board[row][col] = 1;
-                ans += dfs(row+1);
-                board[row][col] = 0;
+        for(int i=0; i <N; i++) {
+            board[row]= i;
+            if (isPossible(row)) {
+                ans += dfs(row +1);
             }
         }
         return ans;
 
     }
 
-    private static boolean isPossible(int row, int col) {
-        for (int i =0; i < row; i++) {
-            if (board[i][col] == 1) {
-                return false;
-            }
-        }
-        int i = row -1, j = col -1;
-        while (0 <= i && 0<=j) {
-            if (board[i][j] == 1) {
-                return false;
-            }
-            i -= 1;
-            j -= 1;
-        }
-
-        i = (row - 1) ;
-        j = (col + 1) ;
-
-        while (0 <= i && j < N) {
-            if (board[i][j] == 1) {
+    private static boolean isPossible(int row) {
+        for (int i = 0; i < row; i++) {
+            if (board[row] == board[i]) {
                 return false;
             }
 
-            i--;
-            j++;
+            if ((row - i) == Math.abs(board[row] - board[i])) {
+                return false;
+            }
         }
+
         return true;
     }
 }
