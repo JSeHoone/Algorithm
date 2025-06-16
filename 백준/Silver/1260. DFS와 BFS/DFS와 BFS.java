d49c2fc5f2b8cd3beca0be5643dffdff99a03ac3
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2025-06-16 16:06:19 +0900

    [Silver II] Title: DFS와 BFS, Time: 280 ms, Memory: 25052 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,98 +1,89 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    private int V;
-    private LinkedList<Integer>[] adj;
-    
-    public void makeGraph(int v) {
-        V = v;
-        adj = new LinkedList[v];
-        for (int i = 0; i < v; i++) {
-            adj[i] = new LinkedList<>();
+    private static int N;
+    private static int M;
+    private static int V;
+    private static LinkedList<Integer>[] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]); // 노드 수
+        M = Integer.parseInt(input[1]); // 간선 수
+        V = Integer.parseInt(input[2]); // 시작 노드
+
+        // make graph
+        graph = new LinkedList[N+1];
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new LinkedList<>();
         }
+
+        // linked edge
+        for (int i=0; i < M; i++) {
+            int[] edge = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+
+        // bfs & dfs
+        StringBuilder sb = new StringBuilder();
+        dfs(sb);
+        sb.append("\n");
+        bfs(sb);
+        System.out.println(sb);
+
     }
-    
-    public void addEdge(int v, int w) {
-        adj[v].add(w);
-        adj[w].add(v);
-    }
-    
-    public void DFS(int s) {
-        boolean[] visited = new boolean[V];
+
+    private static void dfs(StringBuilder sb) {
+        boolean[] visited = new boolean[N+1];
         Stack<Integer> stack = new Stack<>();
-        stack.push(s);
-        
-        while (!stack.isEmpty()) {
+        stack.push(V);
+
+        while(!stack.isEmpty()) {
             int current = stack.pop();
-            
+
             if (!visited[current]) {
                 visited[current] = true;
-                System.out.print((current + 1) + " "); 
-                
+                sb.append(current).append(" ");
 
-                List<Integer> neighbors = new ArrayList<>(adj[current]);
-                Collections.sort(neighbors);
+                // current의 주변 확인
+                List<Integer> neighbors = graph[current];
+                neighbors.sort(Comparator.reverseOrder()); // 작은 순서로 넣기 위함.
 
-                for (int i = neighbors.size() - 1; i >= 0; i--) {
-                    int neighbor = neighbors.get(i);
+                for (int neighbor : neighbors) {
                     if (!visited[neighbor]) {
                         stack.push(neighbor);
                     }
                 }
             }
         }
-    }
-    
 
-    public void BFS(int s) {
-        boolean[] visited = new boolean[V];
-        Queue<Integer> queue = new LinkedList<>();
-        
-        visited[s] = true;
-        queue.add(s);
-        
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            System.out.print((current + 1) + " "); 
-            
-            List<Integer> neighbors = new ArrayList<>(adj[current]);
-            Collections.sort(neighbors);
-            for (int neighbor : neighbors) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    queue.add(neighbor);
+    }
+
+    private static void bfs(StringBuilder sb) {
+        boolean[] visited = new boolean[N+1];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(V);
+
+        while (!q.isEmpty()) {
+            int current = q.poll();
+
+            if (!visited[current]) {
+                visited[current] = true;
+                sb.append(current).append(" ");
+
+                List<Integer> neighbors = graph[current];
+                neighbors.sort(Comparator.naturalOrder()); // 작은 순서로 넣기 위함.
+
+                for (int neighbor : neighbors) {
+                    if (!visited[neighbor]) {
+                        q.add(neighbor);
+                    }
                 }
             }
         }
-    }
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-
-        String[] firstLine = br.readLine().split(" ");
-        int n = Integer.parseInt(firstLine[0]); 
-        int m = Integer.parseInt(firstLine[1]); 
-        int start = Integer.parseInt(firstLine[2]); 
-        
-        Main graph = new Main();
-        graph.makeGraph(n);
-        
-
-        for (int i = 0; i < m; i++) {
-            String[] edge = br.readLine().split(" ");
-            int u = Integer.parseInt(edge[0]) - 1;
-            int v = Integer.parseInt(edge[1]) - 1;
-            graph.addEdge(u, v);
-        }
-        
-
-        int startIndex = start - 1;
-        
-
-        graph.DFS(startIndex);
-        System.out.println();
-        graph.BFS(startIndex);
     }
 }
