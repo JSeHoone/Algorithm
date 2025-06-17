4eedf5e697379835c1613073fbad8e451db157ce
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2025-06-17 17:08:51 +0900

    [Silver I] Title: 미로 탐색, Time: 168 ms, Memory: 16972 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,66 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int col;
-    static int row;
-    // 상 하 좌 우 
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    
-    static class Point {
-        int x, y;
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    
+    private static int N;
+    private static int M;
+    private static int count;
+    private static int[][] board;
+    private static int[] dx = {-1,1,0,0};
+    private static int[] dy = {0,0,-1,1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
-        row = Integer.parseInt(input[0]);
-        col = Integer.parseInt(input[1]);
-        
-        int[][] maze = new int[row][col];
-        int[][] visited = new int[row][col];
-        for (int i = 0; i < row; i++) {
-            Arrays.fill(visited[i], -1);
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+
+        board = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            int[] row = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+            board[i] = row;
         }
-        
-        for (int i = 0; i < row; i++) {
-            int[] line = Arrays.stream(br.readLine().split(""))
-                               .mapToInt(Integer::parseInt)
-                               .toArray();
-            maze[i] = line;
-        }
-        
-        bfs(maze, visited);
-        System.out.print(visited[row - 1][col - 1]);
+
+        bfs();
+        System.out.println(board[N-1][M-1]);
+
     }
-    
-    public static void bfs(int[][] maze, int[][] visited) {
-        Queue<Point> deque = new LinkedList<>();
-        visited[0][0] = 1;
-        deque.offer(new Point(0, 0));
-        
-        while (!deque.isEmpty()) {
-            Point current = deque.poll();
-            int x = current.x;
-            int y = current.y;
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i]; 
-                int ny = y + dy[i]; 
-                
-                if (nx < 0 || nx >= row || ny < 0 || ny >= col) {
-                    continue;
-                }
-                
-                if (maze[nx][ny] == 1 && visited[nx][ny] == -1) {
-                    visited[nx][ny] = visited[x][y] + 1;
-                    deque.offer(new Point(nx, ny));
+
+    private static void bfs() {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0});
+
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+
+            if (board[current[0]][current[1]] != 0) {
+
+                for (int i =0; i < 4; i++) {
+                    int nx = current[0] + dx[i];
+                    int ny = current[1] + dy[i];
+
+                    if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+                        continue;
+                    }
+                    if (board[nx][ny] == 1) {
+                        board[nx][ny] = board[current[0]][current[1]] + 1;
+                        q.add(new int[]{nx,ny});
+                    }
                 }
             }
         }
     }
+
 }
