4a8278b408352cafc5a21b6b6b4233e3765b5b16
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2025-08-29 14:56:04 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/README.md
+++ 프로그래머스/2/12914. 멀리 뛰기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 멀리 뛰기 - 12914 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914#) 
 
 ### 성능 요약
 
-메모리: 85.1 MB, 시간: 0.16 ms
+메모리: 75.1 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 04일 20:45:12
+2025년 08월 29일 14:56:01
 
 ### 문제 설명
 
--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,15 +1,13 @@
 class Solution {
     public long solution(int n) {
         int[] fibo = new int[n+2];
-        
         fibo[1] = 1;
         fibo[2] = 2;
         
         for (int i = 3; i <= n; i++) {
-            fibo[i] = (fibo[i-2] + fibo[i-1]) % 1234567;
+            fibo[i] = (fibo[i-2] + fibo[i-1])%1234567;
         }
-        
-        
+
         return fibo[n];
     }
 }
