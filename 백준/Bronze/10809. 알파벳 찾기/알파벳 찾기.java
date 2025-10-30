188cc846b70ebb3e6091bef3eaf4308584ced752
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2025-10-30 18:51:48 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 120 ms, Memory: 15880 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 알파벳
+        Map<Character, Integer> dict = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            dict.put((char) (i + 'a'), -1);
+        }
+
+        // input word
+        String input = br.readLine();
+        for (int i = 0; i < input.length(); i++) {
+            char alphabet = input.charAt(i);
+            boolean isExist = dict.containsKey(alphabet);
+            if (isExist && dict.get(alphabet) == -1) {
+                dict.put(alphabet, i);
+            }
+        }
+
+        // out
+        for (int value : dict.values()) {
+            System.out.print(value + " ");
+        }
+
+    }
+}
