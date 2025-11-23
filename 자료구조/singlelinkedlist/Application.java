6b90bf19fba184b3bd00e0a871efa198e5cbba9d
JSeHoone <beebea545@gmail.com>
2025-11-23 16:36:18 +0900

    feat : 단일연결리스트 구현
    - add, remove 관련 메서드만 구현함

--- 자료구조/singlelinkedlist/Application.java
+++ 자료구조/singlelinkedlist/Application.java
@@ -1,12 +1,45 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        Node<Integer> newNode = new Node<>(1);
-        Node<Integer> nextNode = new Node<>(2);
 
+        // Node 생성
+        Node<Integer> node1 = new Node<>(1);
+        Node<Integer> node2 = new Node<>(2);
+        Node<Integer> node3 = new Node<>(3);
+        Node<Integer> node4 = new Node<>(4);
+        Node<Integer> node5 = new Node<>(5);
+
+        // LinkedList 생성
         SingleLinkedList<Integer> singleList = new SingleLinkedList<>();
-        singleList.addFirst(newNode);
-        singleList.addFirst(nextNode);
 
+        // 맨 앞에 값을 추가
+        singleList.addFirst(node1);
+        singleList.addFirst(node2);
+        System.out.println(singleList);
+
+        // 맨 뒤에 값을 추가
+        singleList.addLast(node3);
+        System.out.println(singleList);
+
+        // 중간에 값을 추가
+        singleList.add(1, node4);
+        singleList.add(1, node5);
+        System.out.println(singleList);
+
+        // index를 기준으로 값을 삭제
+        System.out.println("==== 삭제 메서드 테스트 ====");
+        // 맨 앞의 값을 삭제
+        singleList.remove(0);
+        System.out.println(singleList);
+
+        // 맨 뒤의 값을 삭제
+        singleList.remove(singleList.length()-1);
+        System.out.println(singleList);
+
+        // 중간의 값을 삭제
+        singleList.remove(1);
         System.out.println(singleList);
 
     }
--- 자료구조/singlelinkedlist/SingleLinkedList.java
+++ 자료구조/singlelinkedlist/SingleLinkedList.java
@@ -5,14 +5,77 @@ public class SingleLinkedList <T> {
     // public method
 
     /**
-     * 맨 앞에 데이터를 추가하는 경우
-     * @param node 추가하고자 하는 Node
+     * 맨 앞에 데이터를 추가하는 로직
+     * @param value 추가하고자 하는 Node
     * */
-    public void addFirst(Node<T> node) {
-        // Head가 넣으려고 하는 node를 바라보도록 하고, 기존의 head node는 next가 된다.
-        Node<T> tempNode = head;
-        head = node;
-        head.next = tempNode;
+    public void addFirst(Node<T> value) {
+        add(0, value);
+    }
+
+    /**
+     * 맨 뒤에 데이터를 추가하는 로직
+     * @param value 추가하고자 하는 Node
+    * */
+    public void addLast(Node<T> value) {
+        add(size,value);
+    }
+
+    /**
+     * 원하는 index내에 데이터를 추가하는 로직
+     * @param index index 위치
+     * @param value 추가하고자 하는 Node
+    * */
+    public void add(int index, Node<T> value) {
+
+        if (index == 0) {
+            // 맨 앞에 값을 추가하는 경우
+            value.next = head;
+            head = value;
+        } else if (index == size) {
+            // 맨 뒤에 값을 추가하는 경우
+            Node<T> findedNode = findNodeByIndex(size-1);
+            findedNode.next = value;
+        } else {
+            // 중간에 값을 추가하는 경우
+            Node<T> preNode = findNodeByIndex(index-1);
+            Node<T> currentNode = findNodeByIndex(index);
+
+            preNode.next = value;
+            value.next = currentNode;
+        }
+
+        size++;
+    }
+
+    /**
+     * 원하는 index에 위치하고 있는 데이터를 삭제하는 로직
+     * @param index index 위치
+    * */
+    public void remove(int index) {
+        if (index == 0) {
+            // 맨 앞의 값을 삭제 하는 경우
+            head = findNodeByIndex(index+1);
+        } else if (index == (size-1)) {
+            // 맨 뒤의 값을 삭제하는 경우
+            Node<T> updatedLastNode = findNodeByIndex(index-1);
+            updatedLastNode.next = null;
+        } else {
+            // 중간에 값을 삭제하는 경우
+            Node<T> preNode = findNodeByIndex(index-1);
+            Node<T> currentNode = findNodeByIndex(index);
+
+            preNode.next = findNodeByIndex(index+1);
+            currentNode.next = null;
+        }
+
+        size--;
+    }
+
+    /**
+     * SingleLinkedList의 크기를 얻기 위한 메서드
+    * */
+    public int length() {
+        return size;
     }
 
     // private method
@@ -42,7 +105,7 @@ public class SingleLinkedList <T> {
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        Node pointer = head;
+        Node<T> pointer = head;
         stringBuilder.append("head").append(" -> ");
         while (null != pointer) {
             stringBuilder.append(pointer.data).append(" -> ");
