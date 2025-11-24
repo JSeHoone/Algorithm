30536b3e04335c17a1189d4b4542e24f06377d28
JSeHoone <beebea545@gmail.com>
2025-11-24 22:26:55 +0900

    feat : double Linked list 구현

---
+++ 자료구조/doublelinkedlist/Application2.java
@@ -0,0 +1,42 @@
+public class Application2 {
+    public static void main(String[] args) {
+        // Node 생성
+        Node2<Integer> node1 = new Node2<>(1);
+        Node2<Integer> node2 = new Node2<>(2);
+        Node2<Integer> node3 = new Node2<>(3);
+        Node2<Integer> node4 = new Node2<>(4);
+        Node2<Integer> node5 = new Node2<>(5);
+
+        // double linked list 생성
+        DoubleLinkedList<Integer> doubleList = new DoubleLinkedList<>();
+
+        // 맨 앞에 값을 추가
+        doubleList.addFirst(node1);
+        doubleList.addFirst(node2);
+        System.out.println(doubleList);
+
+        // 맨 뒤에 값을 추가
+        doubleList.addLast(node3);
+        System.out.println(doubleList);
+
+        // 중간에 값을 추가
+        doubleList.add(1, node4);
+        doubleList.add(1, node5);
+        System.out.println(doubleList);
+
+
+        // index를 기준으로 값을 삭제
+        System.out.println("==== 삭제 메서드 테스트 ====");
+        // 맨 앞의 값을 삭제
+        doubleList.remove(0);
+        System.out.println(doubleList);
+
+        // 맨 뒤의 값을 삭제
+        doubleList.remove(doubleList.length()-1);
+        System.out.println(doubleList);
+
+        // 중간의 값을 삭제
+        doubleList.remove(1);
+        System.out.println(doubleList);
+    }
+}
--- 자료구조/doublelinkedlist/DoubleLinkedList.java
+++ 자료구조/doublelinkedlist/DoubleLinkedList.java
@@ -1,25 +1,75 @@
 public class DoubleLinkedList<T> {
     private Node2<T> head;
     private Node2<T> tail;
-    private int size;
+    private int size = 0;
 
     // public method
     public void add(int index, Node2<T> value) {
         if (index == 0) {
             // 맨 앞에 값을 추가하는 경우
+            value.next = head;
             head = value;
-            value.prev = head;
 
-            tail = value;
-            value.next = tail;
+            tail = value.next;
         } else if (index == size) {
             // 맨 뒤에 값을 추가하는 경우
-            Node2<T> foundNode = findNodeByIndex(size-1);
-            foundNode.next = value;
-            tail = foundNode;
-            value.next = tail;
+            Node2<T> currentTailNode = tail;
+            currentTailNode.next = value;
+            value.prev = currentTailNode;
+            tail = value;
+        } else {
+            // 중간에 값을 추가하는 경우
+            Node2<T> preNode = findNodeByIndex(index-1);
+            Node2<T> currentNode = findNodeByIndex(index);
 
+            preNode.next = value;
+            value.prev = preNode;
+            value.next = currentNode;
+            currentNode.prev = value;
         }
+
+        size++;
+    }
+
+    public void addFirst(Node2<T> value) {
+        add(0, value);
+    }
+
+    public void addLast(Node2<T> value) {
+        add(size, value);
+    }
+
+    public int length() {
+        return size;
+    }
+
+    public void remove(int index) {
+        if (index == 0) {
+            // 맨 앞의 값을 삭제하는 경우
+            Node2<T> currentHeadNode = head;
+
+            head = currentHeadNode.next;
+            currentHeadNode.next = null;
+        } else if (index == (size-1)) {
+            // 맨 뒤의 값을 삭제하는 경우
+            Node2<T> currentLastNode = tail;
+            tail = currentLastNode.prev;
+
+            tail.next = null;
+            currentLastNode.next = null;
+        } else {
+            // 중간에 값을 삭제하는 경우
+            Node2<T> preNode = findNodeByIndex(index-1);
+            Node2<T> currentNode = findNodeByIndex(index);
+            Node2<T> postNode = findNodeByIndex(index+1);
+
+            preNode.next = postNode;
+            postNode.prev = preNode;
+            currentNode.prev = null;
+            currentNode.next = null;
+        }
+
+        size--;
     }
 
 
@@ -29,13 +79,39 @@ public class DoubleLinkedList<T> {
             throw new IndexOutOfBoundsException("Index의 범위를 넘어섰습니다.");
         }
 
-        int nodeIndex = 0;
-        Node2<T> pointer = head;
-        while (nodeIndex != index) {
-            nodeIndex++;
-            pointer = pointer.next;
+        // index가 절반 이상이면 tail에서 찾는 것이 더 효율적 !
+        Node2<T> pointer;
+        if ((index / 2) > size) {
+            int nodeIndex = size-1;
+            pointer = tail;
+            while (nodeIndex != index) {
+                nodeIndex--;
+                pointer = pointer.prev;
+            }
+        } else {
+            int nodeIndex = 0;
+            pointer = head;
+            while (nodeIndex != index) {
+                nodeIndex++;
+                pointer = pointer.next;
+            }
         }
+
         return pointer;
     }
 
+    // to String
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node2<T> pointer = head;
+        sb.append("head").append("->");
+        while (null != pointer) {
+            sb.append(pointer.data).append("->");
+            pointer = pointer.next;
+        }
+
+        sb.append("null");
+        return sb.toString();
+    }
 }
