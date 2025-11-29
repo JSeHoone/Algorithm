742c756458d537ea30ec45f04d0bcae4492c503e
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2025-11-29 15:35:14 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.07 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/README.md
+++ 프로그래머스/1/12931. 자릿수 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자릿수 더하기 - 12931 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12931?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12931) 
 
 ### 성능 요약
 
-메모리: 75 MB, 시간: 0.08 ms
+메모리: 77.6 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 07일 09:04:39
+2025년 11월 29일 15:35:11
 
 ### 문제 설명
 
--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,15 +1,28 @@
 import java.util.*;
+import java.lang.Math;
 
+
+/*
+10의 배수로 몫을 구해서 더해주는 로직
+
+1. 더한 값을 관리하는 변수를 정의한다. (answer)
+2. 지수를 관리하는 변수를 만든다. (exponent)
+3. for문 안에서 n을 10^{exponent}로 나눈 몫을 answer에 더해준다. (8번만 돌면되긴 함)
+*/
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String stringInt = Integer.toString(n);
+        int numSize = (int) Math.log10(n) + 1;
         
-        for (int i = 0; i < stringInt.length(); i++) {
-            char number = stringInt.charAt(i);
-            answer += Character.getNumericValue(number);
-        }
+        for (int expo = numSize-1; expo > -1; expo--) {
+            int unitNum = (int) (n / (Math.pow(10, expo)));
+            n = (int) (n - (Math.pow(10,expo) * unitNum));
+            
+            answer += unitNum;
 
+            
+        }
+        
         return answer;
     }
 }
