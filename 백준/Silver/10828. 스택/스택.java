8f4182d32145d8627b9aa5ecc6804c6004bb6a7c
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2025-12-09 00:15:19 +0900

    [Silver IV] Title: 스택, Time: 276 ms, Memory: 20972 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,101 +1,62 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+/*
+Stack 객체는 오래된 클래스라 synchronized 오버헤드가 많다고 합니다.
+- Vector를 상속하고 있어서 모든 메서드에 불필요한 동기화 비용이 들어가기 때문 !
+- 단일 스레드 환경에서는 완전 비효율적 !
+
+Deque는 Stack 객체보다 빠르고, push/pop 성능이 우수하다고 합니다.
+- 내부 구조는 배열 + 원형 버퍼를 사용한다고 합니다.
+*/ 
 
 public class Main {
-    public static class Node {
-        int value;
-        Node next;
-
-        public Node(int value) {
-            this.value = value;
-            this.next = null;
-        }
-    }
-
-    public static class Stack {
-        private Node head;
-        private int size;
-
-        public Stack() {
-            this.head = null;
-            size = 0;
-        }
-
-        public void push(Node node) {
-            if(this.head == null) {
-                this.head = node;
-            } else {
-                node.next = head;
-                head = node;
-            }
-            size++;
-        }
-
-        public Node pop() {
-            if (this.head == null) {
-                return null;
-            } else {
-                Node tempNode = head;
-                head = head.next;
-                size--;
-                return tempNode;
-            }
-        }
-
-        public int size() {
-            return this.size;
-        }
-
-        public boolean isEmpty() {
-            return this.head == null;
-        }
-
-        public Node top() {
-            return head;
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int N = Integer.parseInt(br.readLine());
-
-        Stack homeMadeStack = new Stack();
+        
+        Deque<Integer> stack = new ArrayDeque<>();
+        
         for (int i = 0; i < N; i++) {
-            String[] command = br.readLine().split(" ");
+            String[] inputs = br.readLine().split(" ");
+            String command = inputs[0];
+            
+            switch (command) {
+                case "push" :
+                    int value = Integer.parseInt(inputs[1]);
+                    stack.push(value);
+                    break;
 
-
-            if (command[0].equals("push")) {
-                int num = Integer.parseInt(command[1]);
-                homeMadeStack.push(new Node(num));
-            } else if (command[0].equals("pop")) {
-                Node node = homeMadeStack.pop();
-
-                if (node==null) {
-                    System.out.println(-1);
-                } else {
-                    System.out.println(node.value);
-                }
-            } else if (command[0].equals("size")) {
-                int length = homeMadeStack.size();
-                System.out.println(length);
-            } else if (command[0].equals("empty")) {
-                boolean isEmpty = homeMadeStack.isEmpty();
-                if (isEmpty) {
-                    System.out.println(1);
-                } else {
-                    System.out.println(0);
-                }
-            } else if (command[0].equals("top")) {
-                Node node = homeMadeStack.top();
-                if (node == null) {
-                    System.out.println(-1);
-                } else {
-                    System.out.println(node.value);
-                }
+                case "pop" :
+                    // if empty
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.pop());
+                    }
+                    break;
+                    
+                case "size" :
+                    System.out.println(stack.size());
+                    break;
+                    
+                case "empty" :
+                    System.out.println(stack.isEmpty() ? 1 : 0);
+                    break;
+                    
+                case "top" :
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.peek());
+                    }
+                    break;
+                    
             }
-        }
-
-
-
+        } 
     }
 }
