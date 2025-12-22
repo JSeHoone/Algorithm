69406944940032e18b8afe465282ccb0db57a8c7
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2025-12-22 22:38:56 +0900

    [level 2] Title: 올바른 괄호, Time: 22.32 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,20 +1,36 @@
+/*
+생각나는 풀이 방법
+1. 스택 자료구조를 이용한다. 
+2. 스택의 peek()를 이용해서 `()`를 판별한다.
+
+*/
+
 import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        for (char ch : s.toCharArray()) {
-            if (ch == '(') {
-                stack.push(ch);
-            } else if (ch == ')') {
-                if (stack.isEmpty()) {
-                    return false;
-                }
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for (char value : s.toCharArray()) {
+            Character peekValue = stack.peek();
+            
+            if (peekValue == null || peekValue == ')') {
+                stack.push(value);
+                continue;
+            } 
+            
+            if (value == ')') {
                 stack.pop();
+            } else {
+                stack.push(value);
             }
+            
         }
-
-        return stack.isEmpty(); 
+        
+        if (stack.size() == 0) {
+            return true;
+        }
+        
+        return false;
     }
 }
