7e08c20370d1e3350447e6222bab21084feb7b7b
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2025-12-29 22:29:26 +0900

    [Silver V] Title: 숫자 카드, Time: 980 ms, Memory: 157980 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,25 +1,25 @@
-/*
-이분 탐색
-정렬하고 중앙 index가 
-
-*/
 import java.util.*;
 import java.io.*;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int N = Integer.parseInt(br.readLine());
-        int[] cards = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        Arrays.sort(cards);
         
-        int testN = Integer.parseInt(br.readLine());
-        int[] testCards = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Set<Integer> cardsSet = new HashSet<>();
+        int[] cards = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        
+        for (int card : cards) {
+            cardsSet.add(card);
+        }
+        
+        int loop = Integer.parseInt(br.readLine());
+        int[] cardsTest = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         
         StringBuilder sb = new StringBuilder();
-        for (int testCard : testCards) {
-            if (isExists(cards, testCard)) {
+        for (int card : cardsTest) {
+            if (cardsSet.contains(card)) {
                 sb.append(1).append(" ");
             } else {
                 sb.append(0).append(" ");
@@ -27,28 +27,5 @@ public class Main {
         }
         
         System.out.println(sb);
-        
     }
-    
-    public static boolean isExists(int[] cards, int target) {
-        int left = 0;
-        int right = cards.length - 1;
-        
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int value = cards[mid];
-            
-            if (value == target) {
-                return true;
-            } else if (value > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        
-        return false;
-    }
-    
-
 }
