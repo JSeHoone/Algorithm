505ef51b050f5162e4c84e8baaa8d64608d3de1e
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2025-12-31 16:06:29 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 696 ms, Memory: 56888 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,34 +1,35 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
         int N = Integer.parseInt(br.readLine());
-        HashMap<String, String> hm = new HashMap<>();
-
+        
+        Map<String, String> people = new HashMap<>();
+        
         for (int i = 0; i < N; i++) {
-            String[] input = br.readLine().split(" ");
-
-            if (hm.containsKey(input[0])) {
-                hm.replace(input[0], input[1]);
+            String[] commands = br.readLine().split(" ");
+            
+            if (commands[1].equals("enter")) {
+                people.put(commands[0], commands[1]);
+            } else {
+                if (people.containsKey(commands[0])) {
+                    people.remove(commands[0]);
+                }
             }
-            hm.putIfAbsent(input[0], input[1]);
         }
-
+        
+        // 사전 순의 역순으로 출력을 해야 함.
+        List<String> peopleList = new ArrayList<>(people.keySet());
+        peopleList.sort(Comparator.reverseOrder());
+        
         StringBuilder sb = new StringBuilder();
-        List<String> response = new LinkedList<>();
-        for (String name : hm.keySet()) {
-            if (hm.get(name).equals("enter")) {
-                response.add(name);
-            }
+        for (String person : peopleList) {
+            sb.append(person).append("\n");
         }
-
-        response.sort(Comparator.reverseOrder());
-        for (String name : response) {
-            sb.append(name).append("\n");
-        }
+        
         System.out.println(sb);
     }
 }
