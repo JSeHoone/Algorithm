e80760d8c5bd46606c3e611a73bb98ed3abbfc2d
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2026-01-05 15:53:09 +0900

    [level 2] Title: 프로세스, Time: 2.82 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,28 +1,51 @@
+/*
+index가 유지되어야 하고, priorities안에 우선순위가 있어야 한다. 
+
+[3,2,2,1] -> 이렇게 정렬을 해두면 좋으려나 ?
+[2,1,3,2] -> 이렇게 되어있을 때 max랑 같이 되어있는지 확인이 필요함
+*/
+
 import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<int[]> queue = new LinkedList<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        for (int i = 0; i < priorities.length; i++) {
-            queue.add(new int[]{priorities[i], i}); // [priority, index]
-            pq.add(priorities[i]);                  // 우선순위만 관리
+        int answer = 0;
+        
+        // Max List를 만들어서 정렬하기
+        Deque<Integer> maxList = new ArrayDeque<>();
+        for (int number : priorities) {
+            maxList.add(number);
         }
-
-        int order = 0;
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            if (cur[0] == pq.peek()) { // 현재 문서가 가장 높은 우선순위
-                pq.poll();
-                order++;
-                if (cur[1] == location) {
-                    return order;
+        
+        List<Integer> sortedList = new ArrayList<>(maxList);
+        sortedList.sort(Comparator.reverseOrder());
+                
+        Deque<Integer> sortedMaxList = new ArrayDeque<>(sortedList);
+        
+        // priorieies를 Deque로 변환
+        Deque<Integer[]> prioritesQueue = new ArrayDeque<>();
+        for (int i = 0; i < priorities.length; i++) {
+            Integer[] data = new Integer[]{i, priorities[i]};
+            prioritesQueue.add(data);
+        }
+        
+        // while을 돌면서
+        while (prioritesQueue.size() != 0) {
+            Integer[] data = prioritesQueue.pollFirst();
+            int max = sortedMaxList.peekFirst();
+        
+            if (data[1] == max) {
+                answer++;
+                sortedMaxList.pollFirst();
+                
+                if (data[0] == location) {
+                    break;
                 }
             } else {
-                queue.add(cur); // 뒤로 보냄
+                prioritesQueue.add(data);
             }
         }
-        return -1;
+        
+        return answer;
     }
 }
