53b92eda9844cd52e3786f0459767dc0ee2e9e93
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2026-01-21 14:43:02 +0900

    [Silver III] Title: N과 M (2), Time: 92 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,45 +1,37 @@
+import java.util.*;
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     private static int N;
     private static int M;
-    private static int[] array;
-    private static boolean[] visited;
-
+    private static int[] result;
+    private static StringBuilder sb = new StringBuilder();
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
+
         N = Integer.parseInt(input[0]);
         M = Integer.parseInt(input[1]);
+        result = new int[M];
 
-        array = new int[N];
-        int number = 1;
-        for (int i = 0; i < N; i++) {
-            array[i] = number;
-            number++;
-        }
-        visited = new boolean[N];
-        StringBuilder sb = new StringBuilder();
-        dfs(0,0, sb);
+        backTracking(1, 0);
         System.out.println(sb);
+        
     }
 
-    static void dfs(int start, int depth, StringBuilder sb) {
+    private static void backTracking(int start, int depth) {
         if (depth == M) {
-            for (int i = 0; i < N; i++) {
-                if (visited[i]) {
-                    sb.append(array[i]).append(" ");
-                }
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = start; i < N; i++) {
-            visited[i] = true;          
-            dfs(i + 1, depth + 1, sb);      
-            visited[i] = false;
+        for (int i = start; i <= N; i++) {
+            result[depth] = i;
+            backTracking(i+1, depth+1);
         }
     }
 }
