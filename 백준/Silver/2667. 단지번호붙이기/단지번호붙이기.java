4af8f024271a9e55d71aaf610d34277b87bec4c0
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2026-01-28 21:43:02 +0900

    [Silver I] Title: 단지번호붙이기, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,71 +1,83 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    // 상 하 좌 우
-    private static final int[] dx = {-1, 1, 0, 0};
-    private static final int[] dy = {0, 0, -1, 1};
+    private static int N;
+    private static int[][] board;
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
+    private static int count;
+    private static List<Integer> countries = new ArrayList<>();
+    
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        int[][] apartment = new int[N][N];
+
+        N = Integer.parseInt(br.readLine());
+        board = new int[N][N];
+
         for (int i = 0; i < N; i++) {
-            int[] row = br.readLine().chars().map(c -> c - '0').toArray();
-            apartment[i] = row;
-        }
-        
-        // BFS
-        List<Integer> countApartment = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
+            int[] row = new int[N];
+            String line = br.readLine();
             for (int j = 0; j < N; j++) {
-                if (apartment[i][j] == 1) {
-                    bfs(apartment, countApartment,i,j);
+                row[j] = line.charAt(j) - '0';
+            }
+            board[i] = row;
+        }
+
+
+        for (int y = 0; y < N; y++) {
+            for (int x = 0; x < N; x++) {
+                if (board[y][x] == 1) {
+                    bfs(x,y);
+                    count++;
                 }
             }
         }
-        
-        StringBuilder response = new StringBuilder();
-        response.append(countApartment.size()).append("\n");
-        // 오름차순 정렬
-        countApartment.sort(Comparator.naturalOrder());
 
-       
-        for (int count : countApartment) {
-            response.append(count).append("\n");
+        StringBuilder sb = new StringBuilder();
+        sb.append(count).append("\n");
+
+        // 오름차순 정렬
+        Collections.sort(countries);
+        for (int country : countries) {
+            sb.append(country).append("\n");
         }
-        
-        System.out.print(response);
+        System.out.println(sb);
     }
-    
-    public static void bfs(int[][] apartment, List<Integer> countApartment, int row, int col) {
-        int groupCount = 1;
-        apartment[row][col] = -1;
-        
-        
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{row,col});
-        
-        while (!queue.isEmpty()) {
-            int[] position = queue.poll();
-            
+
+    private static void bfs(int row, int col) {
+
+        Deque<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{row,col});
+        board[col][row] = 0;
+
+        int country = 1;
+
+        while(!q.isEmpty()) {
+            int[] position = q.pollFirst();
+            int x = position[0];
+            int y = position[1];
+
             for (int i = 0; i < 4; i++) {
-                int nx = position[0] + dx[i];
-                int ny = position[1] + dy[i];
-                
-                if (nx < 0 || nx >= apartment.length || ny < 0 || ny >= apartment[0].length) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
                     continue;
                 }
-                
-                if (apartment[nx][ny] == 1) {
-                    queue.offer(new int[]{nx,ny});
-                    apartment[nx][ny] = -1;
-                    groupCount++;
+
+                if (board[ny][nx] == 1) {
+                    board[ny][nx] = 0;
+                    q.add(new int[]{nx, ny});
+                    country++;
                 }
+                
             }
         }
-        
-        countApartment.add(groupCount);
+
+        countries.add(country);
     }
+
+    
 }
