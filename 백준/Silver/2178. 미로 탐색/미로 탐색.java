9c93cb0ae6f130fa2223f0d9e41966cce0b1a327
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2026-01-31 14:32:43 +0900

    [Silver I] Title: 미로 탐색, Time: 96 ms, Memory: 14556 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,55 +1,73 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    private static int[][] map;
-    private static int x,y;
-    private static int[] dy = {-1,1,0,0};
-    private static int[] dx = {0,0,-1,1};
+/*
+첫째 줄에 N(y-row),M(x-col)이 주어진다.
+출력 : N,M 위치로 가는 최단경로 구하기
 
+-> BFS로 탐색해서 최소의 수를 구하기
+*/
+public class Main {
+    private static int N,M;
+    private static int[][] maze;
+    private static int[] dx = {-1,1,0,0};
+    private static int[] dy = {0,0,-1,1};
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        N = Integer.parseInt(inputs[0]);
+        M = Integer.parseInt(inputs[1]);
 
-        int[] position = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        y = position[0];
-        x = position[1];
+        // create maze
+        maze = new int[N][M];
+        for (int i =0; i < N; i++) {
+            String input = br.readLine();
 
-        map = new int[y][x];
-        for (int i = 0; i < y; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < x; j++) {
-                String token = String.valueOf(line.charAt(j));
-                map[i][j] = Integer.parseInt(token);
+            int[] row = new int[M];
+            for (int j = 0; j < M; j++) {
+                char unit = input.charAt(j);
+                row[j] = (int) unit - '0';
             }
+            maze[i] = row;
         }
 
+        // bfs
         bfs();
-        System.out.println(map[y-1][x-1]);
 
+        // output
+        System.out.println(maze[N-1][M-1]);
     }
 
     private static void bfs() {
-        Queue<int[]> q = new LinkedList<>();
+        Deque<int[]> q = new ArrayDeque<>();
         q.add(new int[]{0,0});
 
-        while (!q.isEmpty()) {
-            int[] position = q.poll();
+        while(!q.isEmpty()) {
+            int[] position = q.pollFirst();
+            int x = position[0];
+            int y = position[1];
 
+            // 현재 위치의 point
+            int point = maze[y][x];
+            if (maze[y][x] == 1) {
+                maze[y][x] = 0;
+            };
+
+            // 상하 좌우 체크
             for (int i = 0; i < 4; i++) {
-                int ny = position[0] + dy[i];
-                int nx = position[1] + dx[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-                if (ny < 0 || ny > map.length - 1 || nx < 0 || nx > map[0].length - 1) {
+                if (nx < 0 || nx >= M || ny < 0 || ny >= N) {
                     continue;
                 }
 
-                if (map[ny][nx] == 1) {
-                    q.add(new int[]{ny,nx});
-                    map[ny][nx] = map[position[0]][position[1]] + 1;
+                if (maze[ny][nx] == 1) {
+                    q.add(new int[]{nx,ny});
+                    maze[ny][nx] = point + 1;
                 }
-
             }
         }
     }
-
 }
