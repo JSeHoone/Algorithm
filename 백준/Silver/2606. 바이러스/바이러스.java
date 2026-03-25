ab10290a876960e54ee1fcc6dfdd07b4567740e9
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2026-03-25 11:24:44 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 14496 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,78 +1,73 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-/*
-첫줄에 컴퓨터 수 주어짐
-두번째 줄에는 네트워크 상에 직접 연결되어있는 컴퓨터 쌍의 수가 주어짐
-이어서 한 쌍씩 주어짐
+class Main {
 
-1번 컴퓨터가 웜 바이러스에 걸렸을 때, 걸리게 되는 컴퓨터의 수를 출력한다.
-*/
-
-public class Main {
-    private static int N,M;
-    private static Map<Integer,List<Integer>> graph = new HashMap<>();
-    private static int count = -1;
+    private static int N;
+    private static int links;
+    private static Map<Integer,List<Integer>> graph;
     private static boolean[] visited;
     
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 그래프 노드를 만들 때 사용
         N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        visited = new boolean[N+1]; // N+1 만큼 할당해서 node번호를 맞춰줌
+        // 엣지를 연결할 때 사용
+        links = Integer.parseInt(br.readLine());
 
-        // create graph
-        for (int i = 0; i < M; i++) {
+        // make graph
+        graph = new HashMap<>();
+        
+        for (int i = 1; i < N+1; i++) {
+            List<Integer> newList = new LinkedList<>();
+            graph.put(i,newList);
+        }
+
+        // link node
+        for (int i = 0; i < links; i++) {
+            // node1, node2 둘다 연결해줘야 함.
             int[] edge = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-            if (graph.containsKey(edge[0])) {
-                graph.get(edge[0]).add(edge[1]);
-            } else {
-                graph.put(edge[0], new ArrayList<>(List.of(edge[1])));
-            }
-
-            if (graph.containsKey(edge[1])) {
-                graph.get(edge[1]).add(edge[0]);
-            } else {
-                graph.put(edge[1], new ArrayList<>(List.of(edge[0])));
-            }
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
         }
 
-
-        // BFS
-        bfs();
-        System.out.print(count);
+        visited = new boolean[N+1];
+        // bfs
+        int answer = bfs(1);
+        System.out.println(answer);
     }
 
-    private static void bfs() {
-        // 무조건 1번 부터 시작
-        Deque<Integer> q = new ArrayDeque<>();
-        q.add(1);
+    private static int bfs(int startNode) {
+
+        Deque<List<Integer>> q = new ArrayDeque<>();
+        q.add(graph.get(startNode));
+        visited[startNode] = true;
+
+        int result = 0;
 
         while(!q.isEmpty()) {
-            int node = q.pollFirst();
+            List<Integer> computers = q.pop();
 
-            if(visited[node]) {
-                continue;
-            }
-            
-            // 방문 처리
-            visited[node] = true;
-            // 방문 컴퓨터 수 +1
-            count++;
-            
-            List<Integer> edges = graph.get(node);
-            if (edges == null) {
-                continue;
-            }
-
-            for (int linkedNode : edges) {
-                if (!visited[linkedNode]) {
-                    q.add(linkedNode);
+            for (int computer : computers) {
+                if (!visited[computer]) {
+                    visited[computer] = true;
+                    result++;
+                } else {
+                    continue;
                 }
+
+                q.add(graph.get(computer));
             }
+            
         }
+
+        return result;
+
+
+        
     }
 }
